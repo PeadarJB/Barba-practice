aba0f01d75446bda46539947c78eb45c94e9c277
Peadar Jolliffe-Byrne <peadarjb@gmail.com>
2023-02-28 16:24:55 -0600

    updating dependencies and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -75,10 +75,9 @@ module.exports = (env, argv) => ({
     }),
   ],
   devServer: {
-    contentBase: [
+    static: [
       path.join(__dirname, '/src'),
     ],
-    watchContentBase: true,
     compress: true,
     port: 9000,
     open: true,
